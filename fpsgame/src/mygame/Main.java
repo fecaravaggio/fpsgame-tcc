42e6296d54a6a0299d2f6f865952c166b1346f8c
Felipe Caravaggio <Flash@Felipe>
2017-11-06 19:13:29 -0200

    Adicionada mira no jogo (método initCrossHairs)

--- fpsgame/src/mygame/Main.java
+++ fpsgame/src/mygame/Main.java
@@ -61,6 +61,7 @@ public class Main extends SimpleApplication {
         initMaterial();
         initFloor(5, 5);
         initWall();
+        initCrossHairs();
         desenhaCena(matriz);
         
     }
@@ -117,10 +118,10 @@ public class Main extends SimpleApplication {
         rootNode.attachChild(chao);  
     }
     
-    public void limiteLabirinto (int x, int y) {
+    public void limiteLabirinto (int i, int j) {
         
-        //x = 4
-        //y = 4
+        //x = 5
+        //y = 5
         
     }
     
@@ -137,8 +138,18 @@ public class Main extends SimpleApplication {
         rootNode.attachChild(paredeHor);
         
     }
-   
     
+    protected void initCrossHairs() {
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        BitmapText ch = new BitmapText(guiFont, false);
+        ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+        ch.setText("+"); // crosshairs
+        ch.setLocalTranslation( // center
+                settings.getWidth() / 2 - guiFont.getCharSet().getRenderedSize() / 3 * 2,
+                settings.getHeight() / 2 + ch.getLineHeight() / 2, 0);
+        guiNode.attachChild(ch);
+    }
+     
     public void desenhaCena(int matriz[][]) {
         
         float x = (float) matriz.length;
