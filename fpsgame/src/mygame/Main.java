fa699dfc34ef8bf8f17c3106121fcbd00c4fa824
fecaravaggio <felipe_797@hotmail.com>
2017-11-26 04:32:57 -0200

    Implementada saída do labirinto

--- fpsgame/src/mygame/Main.java
+++ fpsgame/src/mygame/Main.java
@@ -5,7 +5,6 @@ import com.jme3.asset.TextureKey;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.PhysicsSpace;
 import static com.jme3.bullet.PhysicsSpace.getPhysicsSpace;
-import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
@@ -21,7 +20,6 @@ import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Quaternion;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
@@ -36,7 +34,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -49,6 +46,7 @@ public class Main extends SimpleApplication implements ActionListener {
     Material mat;
     Material matWall;
     Material matFloor;
+    Material matExit;
     Material mat2;
     Box boxParede2;
     Vector3f coordPlayer;
@@ -62,6 +60,8 @@ public class Main extends SimpleApplication implements ActionListener {
     private static Sphere bullet;
     private static SphereCollisionShape bulletCollisionShape;
     private boolean isRunning = true;
+    Vector3f posInicio;
+    Vector3f posExit;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -70,7 +70,6 @@ public class Main extends SimpleApplication implements ActionListener {
 
     @Override
     public void simpleInitApp() {
-
         /**
          * Set up Physics
          */
@@ -93,24 +92,19 @@ public class Main extends SimpleApplication implements ActionListener {
         try {
             loadMap("src\\mygame\\maps\\map1.txt");
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         initFloor(iMap, jMap);
-        coordPlayer = coordInicio();
+        initExit();
 
         CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(0.2f, 0.9f, 1);
         player = new CharacterControl(capsuleShape, 0.05f);
         player.setJumpSpeed(10);
         player.setFallSpeed(10);
         player.setGravity(20);
-        player.setPhysicsLocation(coordPlayer);
-        
-
+        player.setPhysicsLocation(posInicio);      
         bulletAppState.getPhysicsSpace().add(player);
-
     }
-
     /* 
     map2.txt
     
@@ -135,8 +129,8 @@ public class Main extends SimpleApplication implements ActionListener {
     1010111111111111111111111110101110111101
     100000000000001Q000000100000100000000001
     1111111111111111111111111111111111111111
-    
      */
+    
     public void initMaterial() {
 
         matFloor = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
@@ -146,6 +140,12 @@ public class Main extends SimpleApplication implements ActionListener {
         texFloor.setWrap(Texture.WrapMode.Repeat);
         matFloor.setTexture("ColorMap", texFloor);
 
+        matExit = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        Texture texExit = assetManager.loadTexture(
+                "Interface/exit3.jpg");
+        matExit.setTexture("ColorMap", texExit);
+       
         matWall = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         TextureKey keyWall = new TextureKey("Textures/Terrain/BrickWall/BrickWall.jpg");
         keyWall.setGenerateMips(true);
@@ -159,11 +159,9 @@ public class Main extends SimpleApplication implements ActionListener {
         key2.setGenerateMips(true);
         Texture tex2 = assetManager.loadTexture(key2);
         mat2.setTexture("ColorMap", tex2);
-
     }
 
     public void initFloor(int i, int j) {
-
         Box boxChao = new Box((float) i, 0.1f, (float) j);
         Spatial chao = new Geometry("Box", boxChao);
         boxChao.scaleTextureCoordinates(new Vector2f((float) i, (float) j));
@@ -171,7 +169,7 @@ public class Main extends SimpleApplication implements ActionListener {
         chao.setMaterial(matFloor);
         chao.setShadowMode(ShadowMode.Receive);
         chao.setLocalTranslation(i, -0.1f, j);
-        // chao.addControl(new RigidBodyControl(new BoxCollisionShape(new Vector3f((float) i, 0.1f, (float) j)), 0));
+        //chao.addControl(new RigidBodyControl(new BoxCollisionShape(new Vector3f((float) i, 0.1f, (float) j)), 0));
 
         CollisionShape sceneShape
                 = CollisionShapeFactory.createMeshShape(chao);
@@ -184,7 +182,6 @@ public class Main extends SimpleApplication implements ActionListener {
     }
 
     public void initWall(Vector3f ori) {
-
         Spatial paredeBox = new Geometry("Box", boxParede2);
         paredeBox.setMaterial(matWall);
         paredeBox.setLocalTranslation(ori);
@@ -195,13 +192,10 @@ public class Main extends SimpleApplication implements ActionListener {
         paredeBox.addControl(landscape);
 
         bulletAppState.getPhysicsSpace().add(landscape);
-
         rootNode.attachChild(paredeBox);
-
     }
 
     protected void initCrossHairs() {
-
         guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
         BitmapText ch = new BitmapText(guiFont, false);
         ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
@@ -210,29 +204,8 @@ public class Main extends SimpleApplication implements ActionListener {
                 settings.getWidth() / 2 - guiFont.getCharSet().getRenderedSize() / 3 * 2,
                 settings.getHeight() / 2 + ch.getLineHeight() / 2, 0);
         guiNode.attachChild(ch);
-
     }
-
-    public Vector3f coordInicio() {
-        //percorrer a matriz e encontrar o "i" de início do personagem
-
-        String aux = "";
-        Vector3f vt = new Vector3f(0, 0, 0);
-        Tile t;
-
-        for (int i = 0; i < tileArray.size(); i++) {
-            t = tileArray.get(i);
-            //System.out.println ("oh o type do t: " + t.getType());
-            if (t.getType() == 'i') {
-                System.out.println("achei o i");
-                System.out.println("x (i) = " + t.getTileX() + " e y (j) = " + t.getTileY());
-                vt = new Vector3f(1 + t.getTileX() * 2, 2f, 1 + t.getTileY() * 2);
-                break;
-            }
-        }
-        return vt;
-    }
-
+    
     public void anguloInicio() {
         //procurar os elementos vizinhos
         //encontrar 0
@@ -240,17 +213,29 @@ public class Main extends SimpleApplication implements ActionListener {
 
         // n sei se isso vale a pena, mais facil ser carregado junto com o labirinto
     }
+    
+    public boolean detectEndZone () {
+        Vector3f aux = player.getPhysicsLocation();
+        boolean endZone = false;
+        
+        if (aux.x < posExit.x + 1 && aux.x > posExit.x - 1
+                && aux.z < posExit.z + 1 && aux.z > posExit.z -1) {
+            endZone = true;           
+        }
+        else {
+            endZone = false;
+        }
+        return endZone;          
+    }
 
     private void loadMap(String fileName) throws IOException {
         ArrayList lines = new ArrayList();
         int coluna = 0;
         int linha = 0;
-
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
         while (true) {
             String line = reader.readLine();
-            // no more lines to read
             if (line == null) {
                 reader.close();
                 break;
@@ -265,8 +250,6 @@ public class Main extends SimpleApplication implements ActionListener {
         linha = lines.size();
         iMap = linha;
 
-        System.out.println("width/j tem: " + coluna + " e height/i tem: " + linha);
-
         for (int i = 0; i < linha; i++) {
             String line = (String) lines.get(i);
             System.out.println(line);
@@ -274,11 +257,19 @@ public class Main extends SimpleApplication implements ActionListener {
                 char type = line.charAt(j);
                 Tile t = new Tile(i, j, type);
                 tileArray.add(t);
-                //t.tileType();
                 if (type == '1') {
+                    //Parede
                     System.out.println("i = " + i + " j = " + j);
                     Vector3f vt = new Vector3f(+1 + i * 2, 0.5f, +1 + j * 2);
                     initWall(vt);
+                } else if (type == 'i') {
+                    //Posição inicial do jogador
+                   posInicio = new Vector3f(1 + i * 2, 2f, 1 + j * 2);
+                    
+                } else if (type == 'e') {
+                    //Posição da área de saída do jogador
+                    posExit = new Vector3f(1 + i * 2, 1f, 1 + j * 2);
+                    
                 }
             }
         }
@@ -291,48 +282,50 @@ public class Main extends SimpleApplication implements ActionListener {
         inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
         inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
         inputManager.addMapping("Pause", new KeyTrigger(KeyInput.KEY_P));
+        inputManager.addMapping("gc", new KeyTrigger(KeyInput.KEY_X));
+        inputManager.addMapping("shoot", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+        
         inputManager.addListener(this, "Left");
         inputManager.addListener(this, "Right");
         inputManager.addListener(this, "Up");
         inputManager.addListener(this, "Down");
         inputManager.addListener(this, "Jump");
         inputManager.addListener(this, "Pause");
-
-        inputManager.addMapping("shoot", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
-        inputManager.addListener(this, "shoot");
-        inputManager.addMapping("gc", new KeyTrigger(KeyInput.KEY_X));
+        inputManager.addListener(this, "shoot");       
         inputManager.addListener(this, "gc");
     }
     
+    public void initExit () {     
+        Spatial paredeExit = new Geometry("Box", boxParede2);
+        paredeExit.setMaterial(matExit);
+        paredeExit.setLocalTranslation(posExit);      
+        rootNode.attachChild(paredeExit);
+    }
+        
     
     public void textfinishLevel () {    
-        /**
-         * Write text on the screen (HUD)
-         */
         guiNode.detachAllChildren();
-
         guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
-
         BitmapText helloText = new BitmapText(guiFont, false);
-
-        helloText.setSize(guiFont.getCharSet().getRenderedSize());
-
-        helloText.setText("Missão Completa");
-
+        helloText.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+        helloText.setText("Missao Completa!");
         helloText.setLocalTranslation(300, helloText.getLineHeight(), 0);
-
-        guiNode.attachChild(helloText);
-        
+        guiNode.attachChild(helloText);       
     }
     
     @Override
     public void simpleUpdate(float tpf) {
-        Vector3f camDir = cam.getDirection().clone().multLocal(0.05f);
+        //Vector3f camDir = cam.getDirection().clone().multLocal(0.05f);
+        Vector3f camDir = cam.getDirection().clone().multLocal(0.1f);
         Vector3f camLeft = cam.getLeft().clone().multLocal(0.03f);
         walkDirection.set(0, 0, 0);
 
-        System.out.println("o isrunning é: " + isRunning);
-        
+        if (detectEndZone() == true) {
+            //Player encontrou a saída, parar tudo, emitir o log e fechar programa
+            isRunning = !isRunning;
+            textfinishLevel();
+            System.exit(0);
+        }    
         if (left) {
             walkDirection.addLocal(camLeft);
         }
